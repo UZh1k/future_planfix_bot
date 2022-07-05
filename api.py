c9f0517514dcd93da8ae95421f6da0ec7cea6a87
UZh1k <danyanygin4@gmail.com>
2022-07-05 18:09:01 +0300

    webhook added

--- api.py
+++ api.py
@@ -1,1 +1,16 @@
 import config
+import asyncio
+import aiohttp
+
+url = "https://smft.planfix.ru"
+HEADERS = {'User-Agent': 'Chrome/98.0.4758.80'}
+
+
+async def get_html(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, headers=HEADERS) as resp:
+            print(await resp.text())
+
+
+if __name__ == "__main__":
+    asyncio.run(get_html(url))

02b0ac291a2447239b2c3eccb407f35c291676b7
UZh1k <danyanygin4@gmail.com>
2022-07-05 11:25:59 +0300

    bot interface added

--- bot.py
+++ bot.py
@@ -1,6 +1,6 @@
 from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
-from aiogram.types import ParseMode, Message
+from aiogram.types import ParseMode, Message, AllowedUpdates
 from aiogram.utils import executor
 
 import config
@@ -19,13 +19,39 @@ async def send_welcome(message: Message):
     await bot.send_message(message.chat.id, "hello")
 
 
-@dp.message_handler(commands=config.TAGS)
+@dp.message_handler(lambda message: is_group_message(message), commands=config.TAGS)
 async def send_task(message: Message):
     print(message)
-    await bot.send_message(message.chat.id, 'Создание задач в телеграмме '
-                                            'перенос задач в нужную папку '
-                                            'Контроль над задачей '
-                                            'подпункт')
+    await bot.send_message(message.chat.id, ' - Создание задач в телеграмме\n'
+                                            ' - перенос задач в нужную папку\n'
+                                            ' - Контроль над задачей\n'
+                                            '   - подпункт')
+
+
+@dp.message_handler(lambda message: is_group_message(message), commands=map(lambda x: f"connect_{x}", config.TAGS))
+async def connect_task(message: Message):
+    if len(message.text.split()) == 1:
+        await bot.send_message(message.chat.id,
+                               f"Используйте формат {message.text} <id задачи из PlanFix>")
+    else:
+        await bot.send_message(message.chat.id,
+                               f'Привязали группу к задаче #{config.TRANS_DICT[message.text.split()[0].split("connect_")[-1]]}')
+
+
+@dp.message_handler(lambda message: is_group_message(message) and filter_add_task(message))
+async def add_task(message: Message):
+    await bot.send_message(message.chat.id, 'Добавили задачи')
+
+
+def is_group_message(message: Message) -> bool:
+    return message.chat.type == 'group'
+
+
+def filter_add_task(message: Message):
+    for tag in config.TAGS_RU:
+        if message.text.lower().startswith(f"#{tag}"):
+            return True
+    return False
 
 
 async def on_startup(dispatcher: Dispatcher):
@@ -33,4 +59,4 @@ async def on_startup(dispatcher: Dispatcher):
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, on_startup=on_startup, skip_updates=True)
+    executor.start_polling(dp, on_startup=on_startup, skip_updates=True, allowed_updates=AllowedUpdates.all())
--- config.py
+++ config.py
@@ -7,6 +7,11 @@ BOT_TOKEN = os.environ.get("BOT_TOKEN")
 PF_TOKEN = os.environ.get("PF_TOKEN")
 ENDPOINT = "https://smft.planfix.com/rest"
 
-MONTAGE = "pnk"
-SETTING = "mnt"
+MONTAGE = "mnt"
+MONTAGE_RU = "монтаж"
+SETTING = "pnk"
+SETTING_RU = "настройка"
+
 TAGS = (MONTAGE, SETTING)
+TAGS_RU = (MONTAGE_RU, SETTING_RU)
+TRANS_DICT = dict(zip(TAGS, TAGS_RU))

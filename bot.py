ff48ef8f0c7eadc88fff336c4d6dda12b65b9d51
UZh1k <danyanygin4@gmail.com>
2022-07-07 13:02:11 +0300

    rights added

--- bot.py
+++ bot.py
@@ -13,11 +13,20 @@ bot = Bot(token=config.BOT_TOKEN)
 dp = Dispatcher(bot)
 
 
+async def user_is_owner(message: Message):
+    if str(message.from_user.id) in config.OWNER_IDS:
+        return True
+    else:
+        await bot.send_message(message.chat.id, 'Эту функцию могут выполнять только администраторы')
+        return False
+
+
 @dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: Message):
     """
     Слушает /start и /help
     """
+    print(message)
     await bot.send_message(message.chat.id,
                            "/start и /help - выводит список команд\n"
                            "/mount и /tuning <список задач> сохраняет задачи для монтажа или настройки\n"
@@ -51,22 +60,23 @@ async def connect_task(message: Message):
     :param message:
     :return:
     """
-    splitted_message = message.text.split()
-    if len(splitted_message) == 1 or len(splitted_message) > 2 or not splitted_message[1].isdigit():
-        await bot.send_message(message.chat.id,
-                               f"Используйте формат {message.text} <id задачи из PlanFix>")
-    else:
-        task_id = splitted_message[-1]
-        tag = services.get_tag_from_string(splitted_message[0], config.TAGS)
-        if await database.connect_chat_to_task(chat_id=message.chat.id,
-                                               task_id=task_id,
-                                               tag=tag):
+    if await user_is_owner(message):
+        splitted_message = message.text.split()
+        if len(splitted_message) == 1 or len(splitted_message) > 2 or not splitted_message[1].isdigit():
             await bot.send_message(message.chat.id,
-                                   f'Привязали группу к задаче #{config.TRANS_DICT[tag]}')
+                                   f"Используйте формат {message.text} <id задачи из PlanFix>")
         else:
-            await bot.send_message(message.chat.id,
-                                   f'Не вышло привязать группу к задаче #{config.TRANS_DICT[tag]}. '
-                                   f'Перепроверьте данные')
+            task_id = splitted_message[-1]
+            tag = services.get_tag_from_string(splitted_message[0], config.TAGS)
+            if await database.connect_chat_to_task(chat_id=message.chat.id,
+                                                   task_id=task_id,
+                                                   tag=tag):
+                await bot.send_message(message.chat.id,
+                                       f'Привязали группу к задаче #{config.TRANS_DICT[tag]}')
+            else:
+                await bot.send_message(message.chat.id,
+                                       f'Не вышло привязать группу к задаче #{config.TRANS_DICT[tag]}. '
+                                       f'Перепроверьте данные')
 
 
 @dp.message_handler(lambda message: services.is_group_message(message), commands=config.TAGS)
@@ -77,22 +87,23 @@ async def add_task(message: Message):
     :param message:
     :return:
     """
-    splitted_by_n = message.text.split('\n')
-    first_string_splitted = splitted_by_n[0].split()
-    tag = services.get_tag_from_string(first_string_splitted[0], config.TAGS)
-    task_id = database.get_task_id(message.chat.id, tag)
+    if await user_is_owner(message):
+        splitted_by_n = message.text.split('\n')
+        first_string_splitted = splitted_by_n[0].split()
+        tag = services.get_tag_from_string(first_string_splitted[0], config.TAGS)
+        task_id = database.get_task_id(message.chat.id, tag)
 
-    tasks = [" ".join(first_string_splitted[1:])] if len(first_string_splitted) > 1 else []
-    if len(splitted_by_n) > 1:
-        tasks.extend(splitted_by_n[1:])
-    print(tasks)  # таски в листе отправляем в апи
-    every_thing_worked = True
-    for task in tasks:
-        every_thing_worked = every_thing_worked and await api.create_task(task_id, task)
-    if every_thing_worked:
-        await bot.send_message(message.chat.id, f'Добавили пункты для задачи #{config.TRANS_DICT[tag]} {task_id}')
-    else:
-        await bot.send_message(message.chat.id, f'Что-то пошло не так: не все пункты добавлены.')
+        tasks = [" ".join(first_string_splitted[1:])] if len(first_string_splitted) > 1 else []
+        if len(splitted_by_n) > 1:
+            tasks.extend(splitted_by_n[1:])
+        print(tasks)  # таски в листе отправляем в апи
+        every_thing_worked = True
+        for task in tasks:
+            every_thing_worked = every_thing_worked and await api.create_task(task_id, task)
+        if every_thing_worked:
+            await bot.send_message(message.chat.id, f'Добавили пункты для задачи #{config.TRANS_DICT[tag]} {task_id}')
+        else:
+            await bot.send_message(message.chat.id, f'Что-то пошло не так: не все пункты добавлены.')
 
 
 async def on_startup(dispatcher: Dispatcher):

1f42842b6c2a90ba53cb0cfcab6d28f31aac175b
UZh1k <danyanygin4@gmail.com>
2022-08-23 19:59:16 +0300

    fix ban

--- database.py
+++ database.py
@@ -1,25 +1,50 @@
 import psycopg2
+from sqlalchemy import create_engine, Table, Column, Integer, String, MetaData, Boolean, ForeignKey, literal, exists, \
+    DateTime
+from sqlalchemy.sql import select
 
 import config
-import asyncio
 import api
+
+metadata_obj = MetaData()
+
+chat_to_task = Table('chat_to_task_test', metadata_obj,
+                     Column('chat_id', String(255)),
+                     Column('ext_id', String(255)),
+                     Column('int_id', String(255)),
+                     Column('type', String(255))
+                     )
+
+user = Table('user', metadata_obj,
+             Column('id', Integer, primary_key=True),
+             Column('name', String(255)),
+             Column('login_id', String(255)),
+             Column('tg_id', String(255))
+             )
+
+attendance = Table('attendance', metadata_obj,
+                   Column('id', Integer, primary_key=True),
+                   Column('arrived', Boolean),
+                   Column('come', DateTime),
+                   Column('user_id', ForeignKey('user.id')),
+                   )
 
 
 class PostgesOperations:
 
     def __init__(self, DB_URL: str = config.DB_URL):
         self.DB_URL = DB_URL
-        self.connect = psycopg2.connect(config.DB_URL)
-        self.cursor = self.connect.cursor()
+        self.engine = create_engine(config.DB_URL)
+        self.connect = self.engine.connect()
 
     def db_update(func):
         def wrapper(self, *args, **kwargs):
             try:
                 return func(self, *args, **kwargs)
-            except psycopg2.InterfaceError:
-                self.connect = psycopg2.connect(config.DB_URL)
-                self.cursor = self.connect.cursor()
+            except:
+                self.connect = self.engine.connect()
                 return func(self, *args, **kwargs)
+
         return wrapper
 
     @db_update
@@ -28,14 +53,7 @@ class PostgesOperations:
         Создает таблицу, если она еще не была создана
         :return:
         """
-        sql_command = "CREATE TABLE IF NOT EXISTS chat_to_task " \
-                      "(chat_id varchar(255), " \
-                      "ext_id varchar(255), " \
-                      "int_id varchar(255), " \
-                      "type varchar(255));"
-        self.cursor.execute(sql_command)
-        self.connect.commit()
-
+        metadata_obj.create_all(self.engine)
 
     @db_update
     def get_all_rows(self):
@@ -43,9 +61,7 @@ class PostgesOperations:
         Выводит все строки из бд. Нужно для тестов
         :return:
         """
-        self.cursor.execute(f"SELECT * FROM chat_to_task;")
-        return self.cursor.fetchall()
-
+        return self.connect.execute(select(chat_to_task))
 
     @db_update
     async def connect_chat_to_task(self, chat_id, task_id, tag) -> bool:
@@ -58,16 +74,19 @@ class PostgesOperations:
         """
         ext_id = await api.get_int_id(task_id)
         if ext_id:
-            sql_command = f"UPDATE chat_to_task SET ext_id='{ext_id}', int_id='{task_id}' WHERE chat_id='{chat_id}' and type='{tag}';" \
-                          f"INSERT INTO chat_to_task (chat_id, ext_id, int_id, type) " \
-                          f"SELECT '{chat_id}', '{ext_id}', '{task_id}', '{tag}' " \
-                          f"WHERE NOT EXISTS (SELECT 1 FROM chat_to_task WHERE chat_id='{chat_id}' and type='{tag}');"
-            self.cursor.execute(sql_command)
-            self.connect.commit()
+            upd = chat_to_task.update().values(ext_id=ext_id, int_id=task_id).where(
+                chat_to_task.c.chat_id == chat_id and chat_to_task.c.type == tag)
+            self.connect.execute(upd)
+
+            sel = select([literal(chat_id), literal(ext_id), literal(task_id), literal(tag)]).where(
+                ~exists([chat_to_task.c.chat_id]).where(
+                    chat_to_task.c.chat_id == chat_id and chat_to_task.c.type == tag)
+            )
+            ins = chat_to_task.insert().from_select(["chat_id", "ext_id", "int_id", "type"], sel)
+            self.connect.execute(ins)
             return True
         else:
             return False
-
 
     @db_update
     def get_task_id(self, chat_id, tag):
@@ -77,8 +96,10 @@ class PostgesOperations:
         :param tag:
         :return:
         """
-        sql_command = f"SELECT ext_id " \
-                      f"FROM chat_to_task " \
-                      f"WHERE chat_id='{chat_id}' and type='{tag}'"
-        self.cursor.execute(sql_command)
-        return self.cursor.fetchone()[0]
+        query = select(chat_to_task).where(chat_to_task.c.chat_id == chat_id and chat_to_task.c.type == tag)
+        return self.connect.execute(query).fetchone()._mapping[chat_to_task.c.ext_id]
+
+    @db_update
+    def add_user(self, name, login_id, tg_id):
+        return
+
